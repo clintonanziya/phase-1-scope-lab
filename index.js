535a3e94e624c9e975c24449537c96f6704be96e
clintonanziya <clintonanziya3@gmail.com>
2024-03-19 12:49:03 +0300

    completed-phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // Update the customerName variable directly
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error because you cannot reassign a value to a constant.
+}
